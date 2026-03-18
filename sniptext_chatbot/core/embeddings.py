6f37a6bdc45f0d2ca05405a62834ea7de9b2dab8
Azad37-art <azadsultan3377@gmail.com>
2026-03-18 10:18:39 -0700

    Delete sniptext_chatbot/core directory

--- sniptext_chatbot/core/embeddings.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from config.settings import EMBEDDING_MODEL
-
-@st.cache_resource
-def get_embeddings():
-    return HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
